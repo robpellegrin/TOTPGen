539db75b9d9ae41e203990f58ca95fbaae3c56e2
Rob Pellegrin <>
2026-06-05 20:58:26 -0400

    Updated relative imports to package imports. Added totpgen.remove

--- src/totpgen/__init__.py
+++ src/totpgen/__init__.py
@@ -1,3 +1,4 @@
-from .add import add
-from .main import main
-from .totp_store import TotpSecretStore
+from totpgen.add import add
+from totpgen.main import main
+from totpgen.totp_store import TotpSecretStore
+from totpgen.remove import remove
