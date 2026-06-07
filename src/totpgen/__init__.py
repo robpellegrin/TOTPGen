fc85456f18988e3c7806d7164c13efdd105f0680
Rob Pellegrin <>
2026-06-06 22:18:51 -0400

    Linting

--- src/totpgen/__init__.py
+++ src/totpgen/__init__.py
@@ -1,4 +1,4 @@
 from totpgen.add import add
 from totpgen.main import main
-from totpgen.totp_store import TotpSecretStore
 from totpgen.remove import remove
+from totpgen.totp_store import TotpSecretStore
